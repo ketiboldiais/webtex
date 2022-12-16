47080e57c9241d6c6efae3bd7ab4371879e7b7bd
Ketib Oldiais <numerstack@gmail.com>
2022-12-16 10:37:55 -0600

    Add dashboard links and set docs link as index

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -8,19 +8,23 @@ import Register from "./views/Register/Register";
 import Login from "./views/Login/Login";
 import Home from "./views/Home/Home";
 import Docs from "./views/Docs/Docs";
+import { UserHome } from "./views/UserHome/UserHome";
 import { RequireAuth } from "./model/state/RequireAuth";
+import { DashboardLayout } from "./components/DashboardLayout/DashboardLayout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route index element={<Docs />} />
         {/* public routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/docs" element={<Docs />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
         {/* protected routes */}
         <Route element={<RequireAuth />}>
-          <Route path="/home" element={<Home />} />
+          <Route path="dash" element={<DashboardLayout />}>
+            <Route index element={<UserHome />} />
+          </Route>
         </Route>
       </Route>
     </Routes>
