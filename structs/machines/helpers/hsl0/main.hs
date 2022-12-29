599e2a95ed6ff0479362ef22943b7dbfb4077e4f
Ketib Oldiais <numerstack@gmail.com>
2022-12-29 08:13:34 -0600

    finish head, title, and body functions

--- structs/machines/helpers/hsl0/main.hs
+++ structs/machines/helpers/hsl0/main.hs
@@ -1,4 +1,7 @@
-tagHtml content = "<html><body>" <> content <> "</body></html>"
-page = tagHtml "I've been wrapped!"
-
-main = putStrLn page
+main = putStrLn htmlDoc
+htmlDoc = makeHtml "Behold" "I have rendered"
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+html_ content = "<html>" <> content <> "</html>"
+body_ content = "<body>" <> content <> "</body>"
+head_ content = "<head>" <> content <> "</head>"
+title_ content = "<title>" <> content <> "</title>"
