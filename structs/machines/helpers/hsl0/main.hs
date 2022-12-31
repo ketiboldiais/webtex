d9131604a7ea0a49e722df4ddabedbc562ee0f34
Ketib Oldiais <numerstack@gmail.com>
2022-12-31 01:28:03 -0600

    add lurch server module

--- structs/machines/helpers/hsl0/main.hs
+++ structs/machines/helpers/hsl0/main.hs
@@ -1,16 +1,25 @@
 main :: IO ()
 main = putStrLn htmlDoc
+
+
+
 htmlDoc :: [Char]
-htmlDoc = makeHtml "Behold" "I have rendered"
+htmlDoc = makeHtml "Behold" "2 + 2 = " ++ show (2+2)
+
 makeHtml :: [Char] -> [Char] -> [Char]
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
 html_ :: [Char] -> [Char]
 html_ content = "<html>" <> content <> "</html>"
+
 body_ :: [Char] -> [Char]
 body_ content = "<body>" <> content <> "</body>"
+
 head_ :: [Char] -> [Char]
 head_ content = "<head>" <> content <> styles <> "</head>"
+
 styles :: [Char]
 styles = "<link rel=\"stylesheet\" href=\"styles.css\">"
+
 title_ :: [Char] -> [Char]
 title_ content = "<title>" <> content <> "</title>"
---
+++ structs/machines/helpers/lurch/lurch/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lurch
+//
+//  Created by Ketib Oldiais on 12/30/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
