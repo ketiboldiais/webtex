45987b30cb51e4cc9066e158824bc41c8a4ad89b
Ketib Oldiais <numerstack@gmail.com>
2023-01-05 10:45:44 -0600

    add interval and range modules

--- utils/fp/src/index.ts
+++ utils/fp/src/index.ts
@@ -34,22 +34,29 @@ const amendOne = <
 };
 
 /**
- * Calls each callback in the callbacks array.
+ * Executes each function in its argument list, and returns
+ * the results of each call as an array.
+ * @param fs - The functions to execute
+ * @example
+ * ~~~
+  let x = 4;
+  const result = hotline(() => x+1, () => x-2, () => x+4)
+  console.log(result) // [5,2,8]
+ * ~~~
  */
-const hotline = <F extends Function>(...callbacks: F[]) =>
-  callbacks.forEach((f) => f());
+const hotline = <F extends Function>(...fs: F[]) => fs.map((f) => f());
 
 type Obj = { [key: string]: any };
-type MintWorker<A, B, C> = (
+type MintWorker<B, C> = (
   accumulatedValue: B,
-  initialValue: A,
-  shared: C & Obj
+  memo: C & Obj,
+  index: number
 ) => B;
-type Mint = <A, B, C extends Obj>(
-  workers: MintWorker<A, B, C>[],
+type Mint = <B, C extends Obj>(
+  workers: MintWorker<B, C>[],
   accumulatedValue: B,
-  initialValue: A,
-  shared: C
+  memo?: C,
+  index?: number
 ) => B;
 /**
  * Accumulates a value by applying each callback function
@@ -60,46 +67,76 @@ type Mint = <A, B, C extends Obj>(
  * ~~~
  * <A,B>(accumulatedValue: A, initialValue: B) => A
  * ~~~
- *
  * @param accumulatedValue- The accumulated value.
- *
- * @param initialValue - The initial value.
- *
- * @param shared - An optional object to pass shared resources into.
- *
+ * @param memo - An optional object to pass shared resources into.
+ * @param index - The index of the currently executing function
  * @typedef
  * ~~~
  * type Mint = <A,B>(workers: MintWorker<A,B>[], accumulatedValue:B, initialValue:A) => B
  * ~~~
- *
  * @typedef
  * ~~~
  * type MintWorker<A,B> = (accumulatedValue:B, initialValue: A) => B
  * ~~~
- *
  * @example
  * ~~~
- * const j = mint([
- *   (a,i,s) => {
- *     console.log(`(${a},${i}) s.x:${s.x}`) // (3,1) s.x: 14
- *     let x = a + 5
- *     s.w1 = x
- *     return x
- *   },
- *   (a,i,s) => {
- *     console.log(`(${a},${i}) s.y:${s.y}`); // (4,1) s.y: 9
- *     return a - 1
- *   },
- *   (a,i,s) => {
- *     console.log(`(${a},${i}) s.z:${s.z}`) // (5,1) s.z: 12
- *     console.log(`From worker 1: ${s.w1}`) // From worker 1: 8
- *     return a ** 2
- *  }], 3, 1, { x: 14, y: 9, z: 12 });
- * 
- * console.log(j) // 49
+  const j = mint([
+    (a,s) => {
+      console.log(`acc: ${a}`) // acc: 3
+      let x = a + 1;
+      s.w1 = x; // memoize
+      return x;
+    },
+    (a,s,i) => {
+      console.log(`acc: ${a}`) // acc: 3
+      console.log(`s.x: ${s.x}`) // s.x: 2
+      console.log(`i:${i}`) // i:2
+      return a+2;
+    },
+    (a,s) => {
+      console.log(`acc: ${a}`) // acc: 6
+      console.log(`s.w1: ${s.w1}`) // s.w1:4
+      return a+3;
+    }
+  ],3,{ x: 2 });
+
+  console.log(j) // 9
  * ~~~
  */
-const mint: Mint = ([f, ...fs], acc, x, s = <any>{}) =>
-  f === undefined ? acc : mint(fs, f(acc, x, s), x, s);
+const mint: Mint = ([f, ...fs], acc, memo: any) =>
+  f === undefined ? acc : mint(fs, f(acc, memo, fs.length), memo);
 
-export { restruct, amendOne, hotline, mint };
+/**
+ * Generates an integer interval from `start` to `stop` inclusive.
+ * @param start - The first integer.
+ * @param stop - The last integer.
+ * @param step - The increment value (how far apart each integer is from its left and right neighbor).
+ * @example
+ * ~~~
+  const itv = interval(1,3);
+  console.log(itv); // [1,2,3]
+ * ~~~
+ */
+const ag = (start: number, stop: number, step: number, inc = 0) =>
+  Array(Math.ceil((stop + inc - start) / step))
+    .fill(start)
+    .map((x, y) => x + y * step);
+
+const interval = (start: number, stop: number, step = 1) =>
+  ag(start, stop, step, 1);
+
+/**
+ * Generates an integer interval from `start` inclusive to `stop` exclusive.
+ * @param start - The first integer.
+ * @param stop - The last integer.
+ * @param step - The increment value (how far apart each integer is from its left and right neighbor).
+ * @example
+ * ~~~
+  const ivt = interval(0,3);
+  console.log(ivt); // [0,1,2]
+ * ~~~
+ */
+const range = (start: number, stop: number, step = 1) =>
+  ag(start, stop, step, 0);
+
+export { restruct, amendOne, hotline, mint, interval, range };
