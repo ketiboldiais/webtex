77e256f6fd0b41d22a9037d1661d6dcec13f15ea
Ketib Oldiais <numerstack@gmail.com>
2023-01-11 08:39:32 -0600

    add readme for webtex/lang

---
+++ structs/lang/README.md
@@ -0,0 +1,2 @@
+# @webtex/lang
+This is the parser combinator module used in Webtex.
--- structs/lang/src/index.ts
+++ structs/lang/src/index.ts
@@ -1,81 +1,88 @@
-type Result = { type: string; value: any };
-type ParserState = {
+interface Sys {
+  ['string']: string;
+  ['number']: number;
+  ['boolean']: boolean;
+  ['point']: [number, number];
+  ['matrix']: number[][];
+  ['number[]']: number[];
+  ['string[]']: string[];
+  ['object']: { [key: string]: Sys[keyof Sys] };
+  ['{}']: {};
+  ['symbol']: symbol;
+}
+type Result = { type: keyof Sys; value: Sys[keyof Sys] };
+type State = {
   targetString: string;
   index: number;
-  isError: boolean;
+  erred: boolean;
   error: string;
   result: Result;
   results: Result[];
 };
-type ErrorUpdater = (state: ParserState, message: string) => ParserState;
-type ExpectError = (
-  state: ParserState,
-  exp: string,
-  got: string
-) => ParserState;
-type ParseFn = (state: ParserState) => ParserState;
-type str_str = (s: string, i: number, j?: number) => string;
-type str_bool = (s: string) => boolean;
-
-const substr: str_str = (str, i, j) => str.slice(i, j);
-const isEmptyString: str_bool = (str) => str.length === 0;
-
-const newState = <T>(targetObject: T, update: Partial<T>) => {
-  return Object.assign({}, targetObject, update);
-};
-
-const newError: ErrorUpdater = (state, message) =>
-  newState(state, {
-    isError: true,
-    error: message,
-  });
-
-const errEOI = (state: ParserState): ParserState =>
-  newError(state, 'Unexpected end of input.');
-
-const newExpectError: ExpectError = (state, exp, got) =>
-  newError(state, `Expected: ${exp}, got: ${got}`);
-
-type Transformer = (arg1: Result, arg2: Result[]) => Result;
+type ErrorUpdater = (
+  state: State,
+  message: string,
+  parserName?: string,
+  index?: number
+) => State;
+type ParseFn = (state: State) => State;
 type ErrorTransformer = (errorMessage: string, index: number) => string;
 type ChainTransformer = (result: Result) => Parser;
+type Transformer = (arg1: Result, arg2: Result[]) => Result;
+
+const substr = (str: string, i: number, j?: number) => str.slice(i, j);
+
+const isEmptyString = (str: string) => str.length === 0;
+
+const newState = (targetObject: State, update: Partial<State>) =>
+  Object.assign({}, targetObject, update);
+
+const newError: ErrorUpdater = (state, message, parserName = '', i) =>
+  newState(state, {
+    erred: true,
+    error: `Error in ${parserName} @ target[${i}]. ${message}`,
+  });
+
+const newExpectError = (state: State, exp: string, got: string) =>
+  newError(state, `Expected: ${exp}, got: ${got}`);
+
 class Parser {
-  constructor(public transformer: ParseFn) {
+  transformer: ParseFn;
+  constructor(transformer: ParseFn) {
     this.transformer = transformer;
   }
   run(targetString: string) {
-    const initState: ParserState = {
+    const initState: State = {
       targetString,
       index: 0,
       result: { type: 'string', value: '' },
       results: [],
-      isError: false,
+      erred: false,
       error: '',
     };
     return this.transformer(initState);
   }
   map(fn: Transformer) {
-    return new Parser((parserState: ParserState) => {
-      const nextState = this.transformer(parserState);
-      if (nextState.isError) return nextState;
-      // console.log(fn.arguments)
+    return new Parser((state: State) => {
+      const nextState = this.transformer(state);
+      if (nextState.erred) return nextState;
       return newState(nextState, {
         result: fn(nextState.result, nextState.results),
       });
     });
   }
   chain(fn: ChainTransformer) {
-    return new Parser((parserState: ParserState) => {
-      const nextState = this.transformer(parserState);
-      if (nextState.isError) return nextState;
+    return new Parser((state: State) => {
+      const nextState = this.transformer(state);
+      if (nextState.erred) return nextState;
       const nextParser = fn(nextState.result);
       return nextParser.transformer(nextState);
     });
   }
   mapError(fn: ErrorTransformer) {
-    return new Parser((parserState: ParserState) => {
-      const nextState = this.transformer(parserState);
-      if (!nextState.isError) return nextState;
+    return new Parser((state: State) => {
+      const nextState = this.transformer(state);
+      if (!nextState.erred) return nextState;
       return newError(nextState, fn(nextState.error, nextState.index));
     });
   }
@@ -88,11 +95,12 @@ const between =
     );
 
 const char = (str: string) =>
-  new Parser((state: ParserState) => {
-    const { targetString, index, isError } = state;
-    if (isError) return state;
+  new Parser((state: State) => {
+    const { targetString, index, erred } = state;
+    if (erred) return state;
     const target = substr(targetString, index);
-    if (isEmptyString(target)) return errEOI(state);
+    if (isEmptyString(target))
+      return newError(state, 'abrupt end', 'char', state.index);
     if (target.startsWith(str))
       return newState(state, {
         index: index + str.length,
@@ -102,71 +110,63 @@ const char = (str: string) =>
   });
 
 const pRegexer = (regex: RegExp) =>
-  new Parser((state: ParserState) => {
-    const { targetString, index, isError } = state;
-    if (isError) return state;
+  new Parser((state: State) => {
+    const { targetString, index, erred } = state;
+    if (erred) return state;
     const target = targetString.slice(index);
-    if (isEmptyString(target)) return errEOI(state);
+    if (isEmptyString(target))
+      return newError(state, 'abrupt end', 'pRegexer', state.index);
     const match = target.match(regex);
     if (match)
       return newState(state, {
         result: { type: 'string', value: match[0] },
         index: index + match[0].length,
       });
-    return newError(
-      state,
-      `error in letter parser: No match at index ${index}`
-    );
+    return newError(state, `No match.`, 'pRegexer', state.index);
   });
 
 const letters = pRegexer(/^[A-Za-z]+/);
 const digits = pRegexer(/^[0-9]+/);
 
 const sequenceOf = (...parsers: Parser[]) =>
-  new Parser((state) => {
-    if (state.isError) return state;
+  new Parser((state: State) => {
+    if (state.erred) return state;
     let results: Result[] = [];
     let nextState = state;
     for (let p of parsers) {
       nextState = p.transformer(nextState);
       if (nextState.error)
-        return newError(nextState, `Error in sequence parser.`);
+        return newError(nextState, '', 'sequenceOf', nextState.index);
       results.push(nextState.result);
     }
     return newState(nextState, { results });
   });
 
 const choiceOf = (...parsers: Parser[]) =>
-  new Parser((state) => {
-    if (state.isError) return state;
+  new Parser((state: State) => {
+    if (state.erred) return state;
     for (let p of parsers) {
       const nextState = p.transformer(state);
       if (!nextState.error) return nextState;
     }
-    return newError(
-      state,
-      `error in choiceOf parser: No match at index ${state.index}`
-    );
+    return newError(state, `No match.`, 'choiceOf', state.index);
   });
 
 const manyBuilder = (type: 'many' | 'atLeast1') => (parser: Parser) =>
-  new Parser((state: ParserState) => {
-    if (state.isError) return state;
+  new Parser((state: State) => {
+    if (state.erred) return state;
     let nextState = state;
     const results = [];
     let done = false;
     while (!done) {
       let tempState = parser.transformer(nextState);
-      if (!tempState.isError) {
+      if (!tempState.erred) {
         results.push(tempState.result);
         nextState = tempState;
       } else done = true;
     }
     if (type === 'atLeast1' && results.length === 0) {
-      return newError(
-        state,
-        `Error in atLeast1 @ index: ${state.index}. No input matches.`
-      );
+      return newError(state, `No match.`, type, state.index);
     }
     return newState(nextState, { results });
   });
@@ -181,26 +181,47 @@ const bracketed = between(char('['), char(']'));
 
 const braced = between(char('{'), char('}'));
 
-const stringParser = letters.map((res) => ({ type: 'string', value: res }));
-const numParser = digits.map((res) => ({
+const sep = (separator: Parser) => (valueParser: Parser) =>
+  new Parser((state: State) => {
+    if (state.erred) return newError(state, `Died abirth.`, 'sep', state.index);
+    const results: Result[] = [];
+    let nxState = state;
+    while (true) {
+      const targetState = valueParser.transformer(nxState);
+      if (targetState.erred) break;
+      results.push(targetState.result);
+      nxState = targetState;
+      const sepState = separator.transformer(nxState);
+      if (sepState.erred) break;
+      nxState = sepState;
+    }
+    return newState(nxState, { results });
+  });
+
+const stringParser = letters.map((res) => ({
+  type: 'string',
+  value: res.value,
+}));
+
+const numParser = digits.map((res, _) => ({
   type: 'number',
   value: Number(res.value),
 }));
 
 const { log: show } = console;
 
-const tupleParser = sequenceOf(
+const pointParser = sequenceOf(
   char('('),
   numParser,
   char(','),
   numParser,
   char(')')
 ).map((_, results) => ({
-  type: 'tuple',
+  type: 'point',
   value: [Number(results[1].value), Number(results[3].value)],
 }));
 
-const parser = tupleParser;
+const parser = pointParser;
 
 show(parser.run('(1,2)'));
 
@@ -212,6 +233,7 @@ export {
   digits,
   many,
   atLeast1,
+  stringParser,
   parenthesized,
   bracketed,
   braced,
