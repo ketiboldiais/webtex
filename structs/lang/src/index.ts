a6cb5e0f9d373a8a30de8881a55194f26c5b8966
Ketib Oldiais <numerstack@gmail.com>
2023-01-11 15:22:54 -0600

    update between operator

--- structs/lang/src/index.ts
+++ structs/lang/src/index.ts
@@ -1,5 +1,6 @@
 interface Sys {
   ['string']: string;
+  ['word']: string;
   ['number']: number;
   ['boolean']: boolean;
   ['point']: [number, number];
@@ -7,10 +8,18 @@ interface Sys {
   ['number[]']: number[];
   ['string[]']: string[];
   ['object']: { [key: string]: Sys[keyof Sys] };
-  ['{}']: {};
   ['symbol']: symbol;
+  ['operator']: string;
+  ['unary']: { a: Result; op: Result };
+  ['binary']: { a: Result; b: Result; op: Result };
+  ['ternary']: { a: Result; b: Result; c: Result; op: Result };
+  ['keyword']: string;
+  ['expression']: string;
+  ['operation']: any;
+  // ['null']: null;
+  ['{}']: {};
 }
-type Result = { type: keyof Sys | string; value: Sys[keyof Sys] };
+export type Result = { type: keyof Sys; value: Sys[keyof Sys] };
 type State = {
   targetString: string;
   index: number;
@@ -31,10 +40,7 @@ type ChainTransformer = (result: Result) => Parser;
 type Morphism = (arg1: Result, arg2: Result[]) => Result;
 type Update = Partial<State>;
 
-/** Returns a substring from `i` to `j`. */
 const substr = (str: string, i: number, j?: number) => str.slice(i, j);
-
-/** Returns `true` if the string is empty, `false` otherwise. */
 const isBlank = (str: string) => str.length === 0;
 
 /**
@@ -48,7 +54,9 @@ const update = (state: State, change: Update) =>
 const err: ErrorUpdater = (state, msg, parser = '', i) =>
   update(state, {
     erred: true,
-    error: `Error in ${parser} @ target[${i}]. ${msg}`,
+    error: `Error [Parser:${parser}] | str[${i}]: "${
+      state.targetString[state.index]
+    }" | ${msg}`,
   });
 
 const newExpectError = (state: State, exp: string, got: string) =>
@@ -95,6 +103,43 @@ class Parser {
     });
   }
 }
+const expected = (state: State) => state.targetString[state.index];
+const nested =
+  (leftParser: Parser, rightParser: Parser) => (contentParser: Parser) =>
+    new Parser((state: State) => {
+      if (state.erred) return state;
+      let tempState = leftParser.morph(state);
+      if (tempState.erred)
+        return err(
+          tempState,
+          `Left delimiter missing: ${expected(state)}`,
+          'between',
+          tempState.index
+        );
+      tempState = contentParser.morph(tempState);
+      if (tempState.erred)
+        return err(
+          tempState,
+          `delimited content error`,
+          'between',
+          tempState.index
+        );
+      let contentState = tempState;
+      tempState = rightParser.morph(tempState);
+      if (tempState.erred)
+        return err(
+          tempState,
+          `Right delimiter missing: ${expected(state)}`,
+          'between',
+          tempState.index
+        );
+
+      const out = update(contentState, {
+        results: contentState.results,
+        index: tempState.index,
+      });
+      return out;
+    });
 
 const between =
   (leftParser: Parser, rightParser: Parser) => (contentParser: Parser) =>
@@ -102,19 +147,24 @@ const between =
       (_, results) => results[1]
     );
 
-const char = (str: string) =>
+const char = (str: string, type: keyof Sys = 'string') =>
   new Parser((state: State) => {
     const { targetString, index, erred } = state;
     if (erred) return state;
-    const target = substr(targetString, index);
+    const target = targetString.slice(index);
     if (isBlank(target)) return err(state, 'abrupt end', 'char', state.index);
     if (target.startsWith(str))
       return update(state, {
         index: index + str.length,
-        result: { type: 'string', value: str },
+        result: { type, value: str },
       });
     return newExpectError(state, str, substr(target, index, index + 1));
   });
+
+const word = (str: string, type?: keyof Sys) =>
+  sequenceOf(...[...str].map((s) => char(s, type)));
+
+const op = (char: string) => word(char, 'operator');
 
 const pRegexer = (regex: RegExp) =>
   new Parser((state: State) => {
@@ -131,7 +181,7 @@ const pRegexer = (regex: RegExp) =>
       });
     return err(state, `No match.`, 'pRegexer', state.index);
   });
-
+const anyspace = pRegexer(/\s*/);
 const letters = pRegexer(/^[A-Za-z]+/);
 const digits = pRegexer(/^[0-9]+/);
 
@@ -166,7 +216,16 @@ const choiceOf = (...parsers: Parser[]) =>
     }
     return err(state, `No match.`, 'choiceOf', state.index);
   });
-
+const or = (parser1: Parser, parser2: Parser) =>
+  new Parser((state: State) => {
+    const { targetString, index } = state;
+    if (state.erred) return state;
+    const res1 = parser1.run(substr(targetString, index));
+    if (!res1.erred) return res1;
+    const res2 = parser2.run(substr(targetString, index));
+    if (!res2.erred) return res2;
+    return err(state, 'no match found', 'or', state.index);
+  });
 /**
  * Internal helper build for the `many` functions. Not intended to be used directly.
  * @internal
@@ -208,52 +267,51 @@ const parenthesized = between(char('('), char(')'));
 /** Parse everything between braces. */
 const braced = between(char('{'), char('}'));
 
+/** Parse everything between double quotes. */
+const dquoted = between(char('"'), char('"'));
+
+/** Parse everything between single quotes. */
+const squoted = between(char(`'`), char(`'`));
+
 /** Parses symbols separated by the specified separator. */
-const sep =
-  (separator: Parser, type?: string) => (valueParser: Parser) =>
-    new Parser((state: State) => {
-      if (state.erred) return err(state, `Died abirth.`, 'sep', state.index);
-      const results: Result[] = [];
-      let nxState = state;
-      while (true) {
-        const targetState = valueParser.morph(nxState);
-        if (targetState.erred) break;
-        results.push(targetState.result);
-        nxState = targetState;
-        const sepState = separator.morph(nxState);
-        if (sepState.erred) break;
-        nxState = sepState;
-      }
-      return update(nxState, {
-        result: { type: type ? type : nxState.result.type, value: results },
-      });
+const sep = (separator: Parser, type?: keyof Sys) => (valueParser: Parser) =>
+  new Parser((state: State) => {
+    if (state.erred) return err(state, `Died abirth.`, 'sep', state.index);
+    const results: Result[] = [];
+    let nxState = state;
+    while (true) {
+      const targetState = valueParser.morph(nxState);
+      if (targetState.erred) break;
+      results.push(targetState.result);
+      nxState = targetState;
+      const sepState = separator.morph(nxState);
+      if (sepState.erred) break;
+      nxState = sepState;
+    }
+    return update(nxState, {
+      result: { type: type ? type : nxState.result.type, value: results },
     });
+  });
 
 /** Parses a string. */
-const stringParser = letters.map((res) => ({
+const litstring = letters.map((res) => ({
   type: 'string',
   value: res.value,
 }));
 
 /** Parses a number. */
-const numParser = digits.map((res, _) => ({
+const num = digits.map((res, _) => ({
   type: 'number',
   value: Number(res.value),
 }));
 
-const { log: show } = console;
-
 /** Parses a pair. */
-const pointParser = sequenceOf(
-  char('('),
-  numParser,
-  char(','),
-  numParser,
-  char(')')
-).map((_, results) => ({
-  type: 'point',
-  value: [Number(results[1].value), Number(results[3].value)],
-}));
+const point = sequenceOf(char('('), num, char(','), num, char(')')).map(
+  (_, results) => ({
+    type: 'point',
+    value: [Number(results[1].value), Number(results[3].value)],
+  })
+);
 
 type Thunk = () => Parser;
 const lazy = (thunkp: Thunk) =>
@@ -266,34 +324,38 @@ const lazy = (thunkp: Thunk) =>
  * Parse everything between square brackets.
  */
 const bracketed = between(char('['), char(']'));
-
 const numSep = sep(char(','), 'number[]');
 const strSep = sep(char(','), 'string[]');
-
-const numvals = lazy(() => choiceOf(numParser, numtup));
-const strvals = lazy(() => choiceOf(stringParser, strtup));
-
+const numvals = lazy(() => choiceOf(num, numtup));
+const strvals = lazy(() => choiceOf(litstring, strtup));
 const numtup = bracketed(numSep(numvals));
 const strtup = bracketed(strSep(strvals));
-
-const parser = numtup;
-// const parser = strtup;
-
-show(parser.run('[1,2,3,[8,9,10],4,5]'));
-// show(parser.run(`[a,b,[x,y],c]`));
 
 export {
   char,
   sequenceOf,
   choiceOf,
   letters,
+  numtup,
+  strtup,
+  point,
   digits,
   many,
   atLeast1,
-  stringParser,
+  litstring,
+  num,
   parenthesized,
   bracketed,
+  between,
+  nested,
   braced,
   sep,
+  dquoted,
+  squoted,
+  lazy,
   Parser,
+  word,
+  op,
+  anyspace,
+  or,
 };
--- structs/lang/src/run.ts
+++ structs/lang/src/run.ts
@@ -1,7 +1,11 @@
+const { log: show } = console;
+import type { Result } from './index.js';
 import {
   char,
   choiceOf,
   digits,
+  anyspace,
+  op,
   letters,
   sequenceOf,
   many,
@@ -9,9 +13,37 @@ import {
   braced,
   parenthesized,
   bracketed,
+  nested,
+  lazy,
+  between,
+  num,
 } from './index.js';
 
-const { log: show } = console;
+const numberParser = digits.map((x) => ({
+  type: 'number',
+  value: Number(x.value),
+}));
 
-// const parser = sequenceOf(letter);
-// show(parser.run('123'));
+const operatorParser = choiceOf(op('+'), op('-'), op('*'), op('/'));
+const betweenBrackets1 = nested(op('('), op(')'));
+const betweenBrackets2 = between(op('('), op(')'));
+
+const expr1 = lazy(() => choiceOf(numberParser, operationParser1));
+const expr2 = lazy(() => choiceOf(numberParser, operationParser2));
+
+const operationParser1 = betweenBrackets1(
+  sequenceOf(operatorParser, char(' '), expr1, char(' '), expr1)
+).map((result, results) => ({
+  type: 'operation',
+  value: { op: results[0], a: results[2], b: results[4] ? results[4] : result },
+}));
+
+const operationParser2 = betweenBrackets2(
+  sequenceOf(operatorParser, char(' '), expr2, char(' '), expr2)
+).map((result, results) => ({
+  type: 'operation',
+  value: { op: results[0], a: results[2], b: results[4] },
+}));
+
+const input = '(+ (* 18 2) (- 3 2))';
+show(operationParser1.run(input));
